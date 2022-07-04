cc4f65561add0d06e57df0f7470e5b960e1fa037
Hasan Kanaya <hasan.kanaya9@gmail.com>
2022-07-04 18:14:45 +0200

    Test five

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ var swiper = new Swiper(".mySwiper", {
   centeredSlides: true,
   slidesPerView: "auto",
   coverflowEffect: {
-    rotate: 60,
+    rotate: 50,
     stretch: 0,
-    depth: 100,
+    depth: 500,
     modifier: 1,
     slideShadows: true,
   },
